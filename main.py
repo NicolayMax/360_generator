7d440293f7cf442eab484915dd99c84da779d717
Maximov Nikolay <n.d.maximov@gmail.com>
2021-03-22 11:18:17 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+import xlsxwriter
+
+def getRandomUser():
+    return personIds[random.randint(0, 99)]
+
+
+def getAssesmentPersons(personId, count):
+    result = set()
+    while len(result) < count:
+        p = getRandomUser();
+        if (personId != p):
+            result.add(p)
+    return list(result)
+
+
+personFile = open("/Users/nikolaj/Downloads/UserPersonID.txt")
+result = []
+personIds = []
+
+for i in range(0, 100):
+    personIds.append(personFile.readline().replace("\n", ""))
+
+personFile.close()
+
+for person in personIds:
+    assessmentPersons = getAssesmentPersons(person, 20)
+    # result.append([person, person, "1"])
+
+    # for i in range(70):
+    #     result.append([person, assessmentPersons[i], "2"])
+
+    for i in range(0, 20):
+        result.append([person, assessmentPersons[i], "3"])
+
+delimiter = 1
+for number in range(delimiter):
+    workbook = xlsxwriter.Workbook('/Users/nikolaj/Downloads/out_' + str(number) + '.xlsx')
+    worksheet = workbook.add_worksheet("Estimators")
+    page_size = round(len(result) / delimiter)
+    for index in range(page_size):
+        worksheet.write(index, 0, result[index + (number * page_size)][0])
+        worksheet.write(index, 1, result[index + (number * page_size)][1])
+        worksheet.write(index, 2, int(result[index + (number * page_size)][2]))
+    workbook.close()
